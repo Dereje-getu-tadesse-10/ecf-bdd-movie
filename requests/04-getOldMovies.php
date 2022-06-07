ee59d5f754f1bcb7945d58de0e2afdbaa407b2bd
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2022-06-07 11:26:41 +0200

    get film avant 2002

--- requests/04-getOldMovies.php
+++ requests/04-getOldMovies.php
@@ -1,3 +1,11 @@
 <?php
 
+require_once('../connect.php');
+
 // Réaliser une requête qui permet de récupérer tous les films d'avant 2002
+
+$req_get_old_movies = "SELECT * FROM release_years  WHERE release_year < 2002";
+$req_get_old_movies = $db->prepare($req_get_old_movies);
+$req_get_old_movies->execute();
+$old_movies = $req_get_old_movies->fetchAll(PDO::FETCH_ASSOC);
+print_r($old_movies);
