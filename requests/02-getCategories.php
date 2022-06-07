704b118b2a617a108e325551705bc9dd329260d0
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2022-06-07 11:59:40 +0200

    get all info le real la date + le titre

--- requests/02-getCategories.php
+++ requests/02-getCategories.php
@@ -9,3 +9,11 @@ $categories = $req_get_categories->fetchAll(PDO::FETCH_ASSOC);
 // print_r($categories);
 
 // Réaliser une requête qui permet d'afficher le nombre de films par catégories
+
+$req_get_nb_movies_by_category = "SELECT categories.category AS category, COUNT(infos_movies.id) AS nb_movies FROM infos_movies
+INNER JOIN categories ON infos_movies.categories_id = categories.id
+GROUP BY categories.category";
+$req_get_nb_movies_by_category = $db->prepare($req_get_nb_movies_by_category);
+$req_get_nb_movies_by_category->execute();
+$nb_movies_by_category = $req_get_nb_movies_by_category->fetchAll(PDO::FETCH_ASSOC);
+// print_r($nb_movies_by_category);
