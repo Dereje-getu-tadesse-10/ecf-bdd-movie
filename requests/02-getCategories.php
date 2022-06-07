9e17e75cbaace11153f4dfccd69d8b4c2b60be9d
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2022-06-07 11:20:29 +0200

    requete pour obtenir la diff entre le film recent/ancien

--- requests/02-getCategories.php
+++ requests/02-getCategories.php
@@ -2,11 +2,10 @@
 require_once('../connect.php');
 // Réaliser une requête qui permet de récupérer toutes les catégories
 
-// get all categories from category
 $req_get_categories = "SELECT * FROM categories";
 $req_get_categories = $db->prepare($req_get_categories);
 $req_get_categories->execute();
 $categories = $req_get_categories->fetchAll(PDO::FETCH_ASSOC);
-print_r($categories);
+// print_r($categories);
 
 // Réaliser une requête qui permet d'afficher le nombre de films par catégories
--- requests/03-getDifference.php
+++ requests/03-getDifference.php
@@ -1,3 +1,12 @@
 <?php
 
+require_once('../connect.php');
+
 // Réaliser une requête affichant le nombre d'années entre le film le plus récent et le film le plus vieux
+
+$req_get_years = "SELECT release_years_id AS year FROM infos_movies ORDER BY release_years_id DESC ";
+$req_get_years = $db->prepare($req_get_years);
+$req_get_years->execute();
+$years = $req_get_years->fetchAll(PDO::FETCH_ASSOC);
+
+print_r($years);
